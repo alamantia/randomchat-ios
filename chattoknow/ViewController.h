44307ddfe3303e7ee648530f4625d056864fe78e
Anthony LaMantia <anthonyl@well.com>
2012-05-28 22:13:54 -0500

    Initial Commit

---
+++ chattoknow/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  chattoknow
+//
+//  Created by anthony lamantia on 5/28/12.
+//  Copyright (c) 2012 Player2. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
